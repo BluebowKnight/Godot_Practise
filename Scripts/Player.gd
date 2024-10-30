b63f8ca84e406876576d9fa9e06a9512f2fc987c
Bluebow Knight <bulebow_knight@outlook.com>
2024-10-30 13:37:18 +0800

    Note
    Process: Part 4;
    Bug: When player jump, the animation will automatically turn right

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -26,10 +26,12 @@ func player_animations():
 	if Input.is_action_pressed('ui_left') || Input.is_action_just_released('ui_jump'):
 		$AnimatedSprite2D.flip_h = true
 		$AnimatedSprite2D.play("run")
+		$CollisionShape2D.position.x = 7
 		
 	if Input.is_action_pressed('ui_right') || Input.is_action_just_released('ui_jump'):
 		$AnimatedSprite2D.flip_h = false
 		$AnimatedSprite2D.play("run")
+		$CollisionShape2D.position.x = -7
 	
 	if !Input.is_anything_pressed():
 		$AnimatedSprite2D.play("idle")
@@ -40,6 +42,7 @@ func _input(event):
 		$AnimatedSprite2D.play('attack')
 	
 	if event.is_action_pressed("ui_jump") and is_on_floor():
+		velocity.y = jump_height
 		$AnimatedSprite2D.play("jump")
 		
 	if is_climbing == true:
@@ -50,34 +53,6 @@ func _input(event):
 	else:
 		gravity = 200
 		is_climbing = false
-
-'
-const SPEED = 100.0
-const JUMP_VELOCITY = -400.0
-
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
-
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()'
-
 
 func _on_animated_sprite_2d_animation_finished():
 	is_attacking = false # Replace with function body.
