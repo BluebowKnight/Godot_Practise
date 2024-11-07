9845b1203f1e9a05e865e81f514dc068619ac8b2
Bluebow Knight <bulebow_knight@outlook.com>
2024-11-07 13:18:04 +0800

    Note - Add cannon handler

---
+++ Scripts/CannonHandler.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+func _ready():
+	$Body.play("matching")
+
+func _process(delta):
+	$Timer.wait_time = randi_range(1,10)
+	
+	if Global.is_bomb_moving == true:
+		$Body.play("idle")
+		$SpeechBubble.visible = true
+	
+	if Global.is_bomb_moving == false:
+		$Body.play("matching")
+		$SpeechBubble.visible = false
+
+func _on_timer_timeout() -> void:
+	var random_speech = randi() % 3
+	match random_speech:
+		0:
+			$SpeechBubble.play("boom")
+		1:
+			$SpeechBubble.play("loser")
+		2:
+			$SpeechBubble.play("swearing")
